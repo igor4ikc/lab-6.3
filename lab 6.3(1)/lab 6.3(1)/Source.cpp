5076c57132d6afae1b0632cc74d9332f4030a2c7
igor4ikc <igor4ikc@users.noreply.github.com>
2020-11-14 01:35:48 +0200

    first master commit

---
+++ lab 6.3(1)/lab 6.3(1)/Source.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void Min(int* a, const int size, int& min, int& imin, int j)
+{
+	if (min > a[j])
+	{
+		min = a[j];
+		imin = j;
+	}
+	if (j < size - 1)
+		Min(a, size, min, imin, j + 1);
+}
+void Sort(int* a, const int size, int i) 
+{ 
+	int min = a[i];
+	int imin = i;
+	Min(a, size, min, imin, i + 1);
+	a[imin] = a[i];
+	a[i] = min;
+
+	if (i < size - 2)
+		Sort(a, size, i + 1);
+}
+void Print(int* a, const int size, int i)
+{
+	cout << setw(4) << a[i];
+	if (i < size - 1)
+		Print(a, size, i + 1);
+	else
+		cout << endl;
+}
+int main()
+{
+	const int n = 10;
+	int a[n] = { 9, -12, 3, -5, 7, 5, 8, 2, -1, 25 };
+	Print(a, n, 0);
+	Sort(a, n, 0);
+	Print(a, n, 0);
+	return 0;
+}
---
+++ lab 6.3/Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+void Sort(int* a, const int size) 
+{
+	for (int i = 0; i < size - 1; i++) 
+	{
+		int min = a[i]; 
+		int imin = i; 
+		for (int j = i + 1; j < size; j++)
+			if (min > a[j])
+			{
+				min = a[j];
+				imin = j;
+			}
+		a[imin] = a[i]; 
+		a[i] = min; 
+	}
+}
+void Print(int* a, const int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << setw(4) << a[i];
+	cout << endl;
+}
+int main()
+{
+	const int n = 10;
+	int a[n] = { 9, -12, 3, -5, 7, 5, 8, 2, -1, 25 };
+	Print(a, n);
+	Sort(a, n);
+	Print(a, n);
+	return 0;
+}
